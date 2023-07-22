7ebcf61155c4485fa14e35abd55e82ec2325fa41
Kiran Anand <liteninkiran@hotmail.com>
2023-07-22 19:17:15 +0100

    16 - AddTask component and v-model

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
         <Header title="Task Tracker" />
+        <AddTask @add-task="addTask" />
         <Tasks
             @toggle-reminder="toggleReminder"
             @delete-task="deleteTask"
@@ -12,12 +13,14 @@
 <script>
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 export default {
     name: 'App',
     components: {
         Header,
         Tasks,
+        AddTask,
     },
     data() {
         return {
@@ -54,6 +57,9 @@ export default {
         },
         toggleReminder(id) {
             this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task);
+        },
+        addTask(task) {
+            this.tasks = [...this.tasks, task];
         },
     },
 }
