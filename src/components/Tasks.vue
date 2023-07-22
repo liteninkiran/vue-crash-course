774cf619e1847df424dc5db040196f93932548c2
Kiran Anand <liteninkiran@hotmail.com>
2023-07-22 14:49:30 +0100

    12 - Single task component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,20 @@
 <template>
     <div v-for="task in tasks" :key="task.id">
-        <h3>{{ task.text }}</h3>
+        <Task :task="task" />
     </div>
 </template>
 
 <script>
+
+import Task from './Task';
+
 export default {
     name: 'TasksView',
     props: {
         tasks: Array,
+    },
+    components: {
+        Task,
     },
 }
 </script>
