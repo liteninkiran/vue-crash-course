0f7d969dccc06891e380f5534c1aa9d9b211631c
Kiran Anand <liteninkiran@hotmail.com>
2023-07-22 14:19:43 +0100

    06 - Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderView',
+}
+
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
